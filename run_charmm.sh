af59e0bc9a3a774aae750ae90ba8abbfb6eee8dd
hongo10 <ynan@ualberta.ca>
2024-11-04 14:06:48 -0500

    added different charmm version results

--- run_charmm.sh
+++ run_charmm.sh
@@ -2,6 +2,61 @@
 
 #conda activate ommsimu
 
+rm Result_CHARMM.txt
+
+original1="!SODD    CLAD   -0.04762  5.09000 ! test"
+new_COMB="!SODD    CLAD   -0.04762  5.09000 ! test"
+new_NBFIX="SODD    CLAD   -0.04762  5.09000 ! test"
+new_NBTHOLE="!SODD    CLAD   -0.04762  5.09000 ! test"
+new_NBFIX_NBTHOLE="SODD    CLAD   -0.04762  5.09000 ! test"
+
+original2="!SODD    CLAD   1.00000 ! test"
+new2_COMB="!SODD    CLAD   1.00000 ! test"
+new2_NBFIX="!SODD    CLAD   1.00000 ! test"
+new2_NBTHOLE="SODD    CLAD   1.00000 ! test"
+new2_NBFIX_NBTHOLE="SODD    CLAD   1.00000 ! test"
+
+
+CHARMMEXC1=c44b2-serial
+CHARMMEXC2=charmm-c48a2-serial
+CHARMMEXC3=charmm-c48b2-serial-ljpme
+CHARMMEXC4=charmm-c48b2-serial-ljpme-avx
+CHARMMEXC5=charmm-c48b2-serial-ljpme-avx2
+
+for CHARMM in $CHARMMEXC1 $CHARMMEXC2 $CHARMMEXC3 $CHARMMEXC4 $CHARMMEXC5
+do
+
+echo "###################################" >> Result_CHARMM.txt
+echo "# CHARMM Version ${CHARMM}"  >> Result_CHARMM.txt
+echo "###################################" >> Result_CHARMM.txt
+echo "  "                >> Result_CHARMM.txt
+for FF in COMB NBFIX NBTHOLE NBFIX_NBTHOLE
+do
+
+
+cp  ./drude_toppar_nbfix_nbthole_test/toppar_drude_water_nacl_cp.str  ./drude_toppar_nbfix_nbthole_test/toppar_drude_water_nacl.str
+
+
+case $FF in
+     COMB)
+	new_value1="$new_COMB"
+	new_value2="$new2_COMB"
+	;;
+     NBFIX)
+	new_value1="$new_NBFIX"
+	new_value2="$new2_NBFIX"
+	;;
+     NBTHOLE)
+	new_value1="$new_NBTHOLE"
+	new_value2="$new2_NBTHOLE"
+	;;
+     NBFIX_NBTHOLE)
+	new_value1="$new_NBFIX_NBTHOLE"
+	new_value2="$new2_NBFIX_NBTHOLE"
+	 ;;																	        esac
+# Use the determined replacement variables in sed
+sed -i "s|$original1|$new_value1|g" ./drude_toppar_nbfix_nbthole_test/toppar_drude_water_nacl.str
+sed -i "s|$original2|$new_value2|g" ./drude_toppar_nbfix_nbthole_test/toppar_drude_water_nacl.str
 
 for method in switch ljpme
 do
@@ -9,8 +64,24 @@ do
 cd charmm_test
 rm output_*.out
 
-/opt/mackerell/apps/charmm/serial/charmm-c49b1-serial-ljpme -i ener_${method}.inp -o ener_${method}.out
+/opt/mackerell/apps/charmm/serial/$CHARMM -i ener_${method}.inp -o ener_${method}.out
+#/opt/mackerell/apps/charmm/serial/c44b2-serial -i ener_${method}.inp -o ener_${method}_${FF}.out
 
-echo "Generated Output_CHARMM_$method.out"
+
+mv  ./output_charmm_$method.out ./output_charmm_${CHARMM}_${method}_${FF}.out
+
+
+echo "              "
+echo "Running ${CHARMM} ${method} ${FF} ..."
+
+
+#echo "  "  >> ../Result_CHARMM.txt
+echo ">> ${method} ${FF} " >> ../Result_CHARMM.txt
+awk 'NR==11,NR==12' ./output_charmm_${CHARMM}_${method}_${FF}.out >> ../Result_CHARMM.txt
+echo " "  >> ../Result_CHARMM.txt
+#echo "See other details in output_charmm_${method}_${FF}.out"
 cd ../
+done
+
+done
 done
--- run_openmm.sh
+++ run_openmm.sh
@@ -2,6 +2,49 @@
 
 #conda activate ommsimu
 
+rm Result_OPENMM.txt
+
+original1="!SODD    CLAD   -0.04762  5.09000 ! test"
+new_COMB="!SODD    CLAD   -0.04762  5.09000 ! test"
+new_NBFIX="SODD    CLAD   -0.04762  5.09000 ! test"
+new_NBTHOLE="!SODD    CLAD   -0.04762  5.09000 ! test"
+new_NBFIX_NBTHOLE="SODD    CLAD   -0.04762  5.09000 ! test"
+
+original2="!SODD    CLAD   1.00000 ! test"
+new2_COMB="!SODD    CLAD   1.00000 ! test"
+new2_NBFIX="!SODD    CLAD   1.00000 ! test"
+new2_NBTHOLE="SODD    CLAD   1.00000 ! test"
+new2_NBFIX_NBTHOLE="SODD    CLAD   1.00000 ! test"
+
+for FF in COMB NBFIX NBTHOLE NBFIX_NBTHOLE
+do
+
+
+cp  ./drude_toppar_nbfix_nbthole_test/toppar_drude_water_nacl_cp.str  ./drude_toppar_nbfix_nbthole_test/toppar_drude_water_nacl.str
+
+
+case $FF in
+     COMB)
+	new_value1="$new_COMB"
+	new_value2="$new2_COMB"
+	;;
+     NBFIX)
+	new_value1="$new_NBFIX"
+	new_value2="$new2_NBFIX"
+	;;
+     NBTHOLE)
+	new_value1="$new_NBTHOLE"
+	new_value2="$new2_NBTHOLE"
+	;;
+     NBFIX_NBTHOLE)
+	new_value1="$new_NBFIX_NBTHOLE"
+	new_value2="$new2_NBFIX_NBTHOLE"
+	;;
+        esac
+# Use the determined replacement variables in sed
+sed -i "s|$original1|$new_value1|g" ./drude_toppar_nbfix_nbthole_test/toppar_drude_water_nacl.str
+sed -i "s|$original2|$new_value2|g" ./drude_toppar_nbfix_nbthole_test/toppar_drude_water_nacl.str
+
 
 for method in switch ljpme
 do
@@ -20,17 +63,23 @@ val2=$(awk 'NR==9 {print $2}' Output_ene_comp_zero.txt)
 ele_val=$(echo "$val1 - $val2" | bc)
 
 # Print the subtracted result
-#echo "VDW as Output_ene_comp_zero.txt ELE as $subtracted_value"
 
-# Print lines 2 to 9 of Output_ene_comp.txt
-#echo "Printing lines 2-9 of Output_ene_comp.txt:"
-echo "Printing Energy Components for Na-Cl system $method" >  ../output_OPENMM_$method.out
-echo "Distance (A),  Total Energy (kcal/mol)"   >> ../output_OPENMM_$method.out
-awk 'NR>=0 && NR<=8' Output_ene_comp.txt        >> ../output_OPENMM_$method.out
-echo "VDW  $val2"                               >> ../output_OPENMM_$method.out
-echo "ELE  $ele_val"                            >> ../output_OPENMM_$method.out
-awk 'NR>=10 && NR<=12' Output_ene_comp.txt      >> ../output_OPENMM_$method.out
+echo "Running ${method} ${FF} ..."
 
-echo "Generated output_OPENMM_$method.out"
+
+echo "Printing Energy Components for Na-Cl system $method" >  ./output_OPENMM_${method}_$FF.out
+echo "Distance (A),  Total Energy (kcal/mol)"   >> ./output_OPENMM_${method}_$FF.out
+awk 'NR>=0 && NR<=8' Output_ene_comp.txt        >> ./output_OPENMM_${method}_$FF.out
+echo "VDW  $val2"                               >> ./output_OPENMM_${method}_$FF.out
+echo "ELE  $ele_val"                            >> ./output_OPENMM_${method}_$FF.out
+awk 'NR>=10 && NR<=12' Output_ene_comp.txt      >> ./output_OPENMM_${method}_$FF.out
+
+
+echo ">> ${method} ${FF} " >> ../Result_OPENMM.txt
+awk 'NR==11,NR==12' ./output_OPENMM_${method}_${FF}.out >> ../Result_OPENMM.txt
+echo " "  >> ../Result_OPENMM.txt
+
 cd ../
+
+done
 done
