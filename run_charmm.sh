a96d0abf85c3fb6b3c607bcf21161a98e0832df4
hongo10 <ynan@ualberta.ca>
2024-11-04 15:09:11 -0500

    added pass or non-pass

--- run_charmm.sh
+++ run_charmm.sh
@@ -41,19 +41,27 @@ case $FF in
      COMB)
 	new_value1="$new_COMB"
 	new_value2="$new2_COMB"
+        b_vdw=2.152
+	b_ele=-123.072
 	;;
      NBFIX)
 	new_value1="$new_NBFIX"
 	new_value2="$new2_NBFIX"
+        b_vdw=58.582
+	b_ele=-123.072
 	;;
      NBTHOLE)
 	new_value1="$new_NBTHOLE"
 	new_value2="$new2_NBTHOLE"
+	b_vdw=2.152
+	b_ele=-106.773
 	;;
      NBFIX_NBTHOLE)
 	new_value1="$new_NBFIX_NBTHOLE"
 	new_value2="$new2_NBFIX_NBTHOLE"
-	 ;;																	        esac
+	b_vdw=58.582
+	b_ele=-106.773
+	;;																	        esac
 # Use the determined replacement variables in sed
 sed -i "s|$original1|$new_value1|g" ./drude_toppar_nbfix_nbthole_test/toppar_drude_water_nacl.str
 sed -i "s|$original2|$new_value2|g" ./drude_toppar_nbfix_nbthole_test/toppar_drude_water_nacl.str
@@ -74,10 +82,34 @@ mv  ./output_charmm_$method.out ./output_charmm_${CHARMM}_${method}_${FF}.out
 echo "              "
 echo "Running ${CHARMM} ${method} ${FF} ..."
 
+if [  -s ./output_charmm_${CHARMM}_${method}_${FF}.out ]; then
+
+val2=$(awk 'NR==11 {print $2}' ./output_charmm_${CHARMM}_${method}_${FF}.out)
+ele_val=$(awk 'NR==12 {print $2}' ./output_charmm_${CHARMM}_${method}_${FF}.out)
+
+diff1=$(echo "scale=5; if ($val2 > $b_vdw) $val2 - $b_vdw else $b_vdw - $val2" | bc)
+diff2=$(echo "scale=5; if ($ele_val > $b_ele) $ele_val - $b_ele else $b_ele - $ele_val" | bc)
+
+if (( $(echo "$diff1 < 0.1" | bc -l) )); then
+	result1="pass"
+else
+	result1="!!!!non-pass"
+fi
+
+if (( $(echo "$diff2 < 0.1" | bc -l) )); then
+	result2="pass"
+else
+        result2="!!!!non-pass"
+fi
+fi
 
 #echo "  "  >> ../Result_CHARMM.txt
 echo ">> ${method} ${FF} " >> ../Result_CHARMM.txt
-awk 'NR==11,NR==12' ./output_charmm_${CHARMM}_${method}_${FF}.out >> ../Result_CHARMM.txt
+awk -v val1="$result1" -v val2="$result2" '
+NR==11 {print $0, val1}
+NR==12 {print $0, val2}
+' ./output_charmm_${CHARMM}_${method}_${FF}.out >> ../Result_CHARMM.txt
+
 echo " "  >> ../Result_CHARMM.txt
 #echo "See other details in output_charmm_${method}_${FF}.out"
 cd ../
--- run_openmm.sh
+++ run_openmm.sh
@@ -27,18 +27,26 @@ case $FF in
      COMB)
 	new_value1="$new_COMB"
 	new_value2="$new2_COMB"
+	b_vdw=2.152
+	b_ele=-123.072
 	;;
      NBFIX)
 	new_value1="$new_NBFIX"
 	new_value2="$new2_NBFIX"
+	b_vdw=58.582
+	b_ele=-123.072
 	;;
      NBTHOLE)
 	new_value1="$new_NBTHOLE"
 	new_value2="$new2_NBTHOLE"
+	b_vdw=2.152
+	b_ele=-106.773
 	;;
      NBFIX_NBTHOLE)
 	new_value1="$new_NBFIX_NBTHOLE"
 	new_value2="$new2_NBFIX_NBTHOLE"
+	b_vdw=58.582
+	b_ele=-106.773
 	;;
         esac
 # Use the determined replacement variables in sed
@@ -66,12 +74,29 @@ ele_val=$(echo "$val1 - $val2" | bc)
 
 echo "Running ${method} ${FF} ..."
 
+diff1=$(echo "scale=5; if ($val2 > $b_vdw) $val2 - $b_vdw else $b_vdw - $val2" | bc)
+diff2=$(echo "scale=5; if ($ele_val > $b_ele) $ele_val - $b_ele else $b_ele - $ele_val" | bc)
+
+if (( $(echo "$diff1 < 0.01" | bc -l) )); then
+    result1="pass"
+else
+    result1="!!!!non-pass"
+fi
+
+
+if (( $(echo "$diff2 < 0.01" | bc -l) )); then
+    result2="pass"
+else
+    result2="!!!!non-pass"
+fi
+
+
 
 echo "Printing Energy Components for Na-Cl system $method" >  ./output_OPENMM_${method}_$FF.out
 echo "Distance (A),  Total Energy (kcal/mol)"   >> ./output_OPENMM_${method}_$FF.out
 awk 'NR>=0 && NR<=8' Output_ene_comp.txt        >> ./output_OPENMM_${method}_$FF.out
-echo "VDW  $val2"                               >> ./output_OPENMM_${method}_$FF.out
-echo "ELE  $ele_val"                            >> ./output_OPENMM_${method}_$FF.out
+echo "VDW  $val2   $result1"                    >> ./output_OPENMM_${method}_$FF.out
+echo "ELE  $ele_val   $result2"                 >> ./output_OPENMM_${method}_$FF.out
 awk 'NR>=10 && NR<=12' Output_ene_comp.txt      >> ./output_OPENMM_${method}_$FF.out
 
 
